8d21f68ce329677a293fca352acd7915f56b0ccf
Ryan Walmsley <Ryan@pi-supply.com>
2020-12-27 20:06:38 +0000

    I just can't get this to work in a container.

--- gateway-config/start-gateway-config.sh
+++ gateway-config/start-gateway-config.sh
@@ -4,14 +4,11 @@ sleep 15
 echo "Starting connmand"
 connmand
 
-
 sleep 15
 echo "Starting gateway_config"
 /opt/gateway_config/bin/gateway_config start
 
-
-
-sleep 15
+sleep 30
 echo "advertise on"
 
 /opt/gateway_config/bin/gateway_config advertise on
