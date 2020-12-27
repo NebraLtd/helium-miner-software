8cd3bd277d44b780a324fe57dfe0aa896ea14324
Ryan Walmsley <Ryan@pi-supply.com>
2020-12-27 19:49:47 +0000

    Change config slightly.

--- gateway-config/start-gateway-config.sh
+++ gateway-config/start-gateway-config.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-sleep 30
+sleep 15
+echo "Starting connmand"
+connmand
+
+
+sleep 15
+echo "Starting gateway_config"
 /opt/gateway_config/bin/gateway_config start
 
-sleep 30
+
+
+sleep 15
+echo "advertise on"
 
 /opt/gateway_config/bin/gateway_config advertise on
 
