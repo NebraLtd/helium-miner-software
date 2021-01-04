203c2d55cbdbdd574f9cce62d4d6a6a31a7750c3
Ryan Walmsley <Ryan@ryanteck.uk>
2021-01-04 19:17:45 +0000

    More yeeeeeeeting

--- gateway-config/start-gateway-config.sh
+++ gateway-config/start-gateway-config.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-sleep 15
-echo "Starting connmand"
-#connmand --device wlan0
-
-sleep 15
-echo "Starting gateway_config"
-/opt/gateway_config/bin/gateway_config start
-
-sleep 30
-echo "advertise on"
-
-/opt/gateway_config/bin/gateway_config advertise on
-
 sleep 5
 
 while true; do sleep 1; done
