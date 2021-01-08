64da89c2adcdfebdd0e10bf5ad62dd6fb6d8b641
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-08 18:05:06 +0000

    lets see

--- pktfwd/buildfiles/compileSX1302.sh
+++ pktfwd/buildfiles/compileSX1302.sh
@@ -10,5 +10,13 @@ cd /opt/iotloragateway/dev/sx1302_hal-1.0.5
 
 #Remove old files
 
+rm libloragw/inc/loragw_stts751.h -f
+rm libloragw/src/loragw_stts751.c -f
+
+#Copy new files
+cp ../sx1302fixes/loragw_hal.c libloragw/src/loragw_hal.c -f
+cp ../sx1302fixes/Makefile libloragw/Makefile -f
+cp  ../sx1302fixes/lora_pkt_fwd.c packet_forwarder/src/lora_pkt_fwd.c
+
 make clean
 make -j 4
