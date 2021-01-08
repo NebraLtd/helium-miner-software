40c40be4fc79f3a3032645bd9f94900a65bd4176
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-08 18:03:26 +0000

    Progress

--- pktfwd/buildfiles/compileSX1302.sh
+++ pktfwd/buildfiles/compileSX1302.sh
@@ -7,5 +7,8 @@ wget https://github.com/NebraLtd/sx1302_hal/archive/V1.0.5.tar.gz
 tar -xzvf V1.0.5.tar.gz
 
 cd /opt/iotloragateway/dev/sx1302_hal-1.0.5
+
+#Remove old files
+
 make clean
 make -j 4
