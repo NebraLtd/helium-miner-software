30fc2ef9be84ca143419cc4d11b98c247feabdb2
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-08 16:51:08 +0000

    Try all of that

--- pktfwd/buildfiles/compileSX1302.sh
+++ pktfwd/buildfiles/compileSX1302.sh
@@ -2,8 +2,10 @@
 
 cd /opt/iotloragateway/dev
 
-git clone https://github.com/NebraLtd/sx1302_hal.git
+#git clone https://github.com/NebraLtd/sx1302_hal.git
+wget https://github.com/NebraLtd/sx1302_hal/archive/V1.0.5.tar.gz
+tar -xzvf V1.0.5.tar.gz
 
-cd /opt/iotloragateway/dev/sx1302_hal
+cd /opt/iotloragateway/dev/sx1302_hal-1.0.5
 make clean
 make -j 4
