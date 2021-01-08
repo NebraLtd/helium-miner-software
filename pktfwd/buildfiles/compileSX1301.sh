36d8e817f9bce6161a2b5ef0a3b169f4363e5b56
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-08 13:00:49 +0000

    Start re-writing sx1301 too.

--- pktfwd/buildfiles/compileSX1301.sh
+++ pktfwd/buildfiles/compileSX1301.sh
@@ -5,11 +5,8 @@ mkdir -p /opt/iotloragateway/dev
 mkdir -p /var/www/iotloragateway
 cd /opt/iotloragateway/dev
 
-git clone https://github.com/PiSupply/lora_gateway.git
-git clone https://github.com/PiSupply/paho.mqtt.embedded-c.git
-git clone https://github.com/PiSupply/ttn-gateway-connector.git
-#git clone https://github.com/PiSupply/protobuf-c.git
-git clone https://github.com/PiSupply/packet_forwarder.git
+git clone https://github.com/NebraLtd/lora_gateway.git
+git clone https://github.com/NebraLtd/packet_forwarder.git
 git clone https://github.com/PiSupply/iot-lora-controller.git
 
 cd /opt/iotloragateway/dev/lora_gateway/libloragw
@@ -17,21 +14,8 @@ make clean
 sed -i -e 's/PLATFORM= .*$/PLATFORM= iotloragw_standalone0/g' library.cfg
 make -j 4
 
-
-cd /opt/iotloragateway/dev/paho.mqtt.embedded-c
-make -j 4
-make install
-
-echo "TTN Connector"
-cd /opt/iotloragateway/dev/ttn-gateway-connector
-cp config.mk.in config.mk
-make -j 4
-cp /opt/iotloragateway/dev/ttn-gateway-connector/bin/libttn-gateway-connector.so /usr/lib
-
-
 echo "Packet Forwarder"
-cd /opt/iotloragateway/dev/packet_forwarder/mp_pkt_fwd
+cd /opt/iotloragateway/dev/packet_forwarder/
 make clean
-git checkout docker-sg0
 make -j 4
-cp /opt/iotloragateway/dev/packet_forwarder/mp_pkt_fwd/mp_pkt_fwd /opt/iotloragateway/packetforwarder_sg0
+cp /opt/iotloragateway/dev/packet_forwarder/lora_pkt_fwd/lora_pkt_fwd /opt/iotloragateway/packetforwarder_sg0
