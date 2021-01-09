bc59d5948552f23d7aa4a52e5420d065d582165a
Ryan Walmsley <ryan@pi-supply.com>
2021-01-09 23:23:12 +0000

    Basis

---
+++ diagnostics/diagnosticsProgram.py
@@ -0,0 +1,12 @@
+#Checks basic hardware features.
+
+import os
+
+#Variables for all Checks
+
+ethernetMacAddress = ""
+wifiMacAddress = ""
+balenaName = ""
+eccDetected = ""
+rpiSerialNumber = ""
+loraTest = False
