af6f9282b981200eb1fbd7f62d2a9ed5008743ca
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-10 16:13:37 +0000

    Try that.

--- diagnostics/genHTML.py
+++ diagnostics/genHTML.py
@@ -1,1 +1,12 @@
 #The code that generates the HTML
+def generateHTML(jsonString):
+    htmlData = """
+<html>
+    <head>
+        <title>NEBRA HELIUM HOTSPOT DIAGNOSTICS</title>
+    </head>
+    <body>
+
+    </body>
+</html>
+    """
