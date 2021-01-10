68fb6d3095c9a52ca97abdc377ca9d2cc39f6275
Ryan Walmsley <ryan@pi-supply.com>
2021-01-10 22:57:59 +0000

    Add name

--- diagnostics/genHTML.py
+++ diagnostics/genHTML.py
@@ -52,6 +52,7 @@ def generateHTML(dictString):
   <div class="row">
     <div class="col">
       <h1 class="text-center">Diagnostics Information</h1>
+      <h2>Name: %(BN)s</h2>
 """
     if(dictString["ecc"] == True and dictString["E0"] != "FF:FF:FF:FF:FF:FF" and dictString["W0"] != "FF:FF:FF:FF:FF:FF" and dictString["BT"] == True and  dictString["LOR"] == True):
         htmlData = htmlData + """<h2 class="text-success text-center">All Ok</h2>"""
