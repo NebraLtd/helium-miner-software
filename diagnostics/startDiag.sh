f2436f4e701294a15aad43fd72ce3aa52c6e1aca
Ryan Walmsley <Ryan@pi-supply.com>
2021-01-20 14:37:27 +0000

    Change diagnostics to work better

--- diagnostics/startDiag.sh
+++ diagnostics/startDiag.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #Run diag
+rm /opt/nebraDiagnostics/html/index.html
+cp /opt/nebraDiagnostics/html/index.html.template /opt/nebraDiagnostics/html/index.html
 sleep 90
 rm /opt/nebraDiagnostics/html/index.html
 python3 -u /opt/nebraDiagnostics/diagnosticsProgram.py
