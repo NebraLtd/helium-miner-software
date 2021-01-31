5034e8a5afa1d1be7551c5e83ecad23892eb8396
Ryan Walmsley <ryan@ryanteck.uk>
2021-01-31 19:09:49 +0000

    Slightly bigger QR

--- diagnostics/diagnosticsProgram.py
+++ diagnostics/diagnosticsProgram.py
@@ -131,7 +131,7 @@ while True:
     qrcodeBytes = qrcodeJson.encode('ascii')
     qrcodeBase64 = base64.b64encode(qrcodeBytes)
     qrcodeOut = qrcode.make(qrcodeBase64)
-    qrcodeOut = qrcodeOut.resize((600, 600), Image.ANTIALIAS)
+    qrcodeOut = qrcodeOut.resize((625, 625), Image.ANTIALIAS)
 
     canvas = Image.new('RGBA', (675, 800), (255,255,255,255))
 
