7a9552f20e4036129d484ddb09e0d141db2601e7
Ryan Walmsley <ryan@ryanteck.uk>
2021-02-06 13:16:31 +0000

    Remove all old files no longer needed.

--- diagnostics/startDiag.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#Run diag
-rm /opt/nebraDiagnostics/html/index.html
-cp /opt/nebraDiagnostics/html/index.html.template /opt/nebraDiagnostics/html/index.html
-python3 -m http.server 80 &
-sleep 90
-rm /opt/nebraDiagnostics/html/index.html
-python3 -u /opt/nebraDiagnostics/diagnosticsProgram.py
--- miner/start-miner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PUBLIC_KEYS=$(/opt/miner/bin/miner print_keys)
-[ $? -ne 0 ] && exit 1
-echo $PUBLIC_KEYS > /var/data/public_keys
-
-/opt/miner/bin/miner foreground
