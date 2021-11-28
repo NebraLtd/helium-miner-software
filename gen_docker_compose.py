777d5f0c7decdd098123a2f9b97fde06f1af43e4
Kashif Iftikhar <kashif@compulife.com.pk>
2021-11-29 00:45:24 +0500

    Support for device type
    - Instead of asking for arch, ask for device type (currently rpi
      or rockpi).
    
    - Sections in settings.ini for supported device types containing
      values for device specific settings like ARCH and I2C_DEVICE.

--- gen_docker_compose.py
+++ gen_docker_compose.py
@@ -43,19 +43,33 @@ class DockerComposer:
         self.config = config
 
     def generate_compose_file(
-            self, template_file: str, output_file: str, arch: str) -> None:
+            self,
+            device_type: str,
+            template_file: str,
+            output_file: str
+    ) -> None:
         """generate_compose_file Render template_file to generate compose file.
 
         Args:
+            device_type (str): The target device for the docker compose file.
+                This device should have a section in settings.ini.
             template_file (str): Template file name without folder name.
                 Should be present in the templates folder.
             output_file (str): Output filename. Can be a path. Otherwise
                 file is created in current folder.
         """
         template = self.jinja_env.get_template(template_file)
-        template_args = {'ARCH': arch}
+        if not self.config.has_section(f'device_{device_type}'):
+            print(f"Error: Unknown device type: {device_type}. "
+                  "No matching config section found in settings.ini")
+            sys.exit(1)
+
+        template_args = {}
         assert 'versions' in self.config
         for k, v in self.config['versions'].items():
+            template_args[k] = v
+
+        for k, v in self.config[f'device_{device_type}'].items():
             template_args[k] = v
 
         output = template.render(**template_args)
@@ -66,15 +80,17 @@ class DockerComposer:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser("Generate docker-compose.yml.")
-    parser.add_argument('--arch', '-a', default='arm64',
-                        help="Target architecture.")
+    parser.add_argument('device_type',
+                        help="Target device. Must have a section in "
+                             "settings.ini.")
     parser.add_argument('--template', '-t', default='docker-compose.template',
-                        help=("Input template file. Should be present in "
-                              "templates folder."))
+                        help="Input template file. Should be present in "
+                             "templates folder.")
     parser.add_argument('--output', '-o', default='docker-compose.yml',
                         help="Output file. Created in current folder.")
     args = parser.parse_args()
     # print(args)
 
     composer = DockerComposer()
-    composer.generate_compose_file(args.template, args.output, args.arch)
+    composer.generate_compose_file(
+        args.device_type, args.template, args.output)
