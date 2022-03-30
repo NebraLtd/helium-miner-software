c7ab0afea6467237f0b4e0db2ff483ae1349e369
Murat Ursavas <mursavas@nebra.com>
2022-03-30 14:46:27 +0300

    Added automation for light hotspots, removed different fleet necessity for every device type.
    Removed necessity for upfront I2C device report. Now a single release could be used for all of the devices.
    (NebraLtd/helium-miner-software#399)

--- gen_docker_compose.py
+++ gen_docker_compose.py
@@ -15,7 +15,6 @@ For the time being to avoid adding dependencies to the project it just uses pyth
 format strings.
 """
 
-import sys
 import os
 import argparse
 from typing import Union
@@ -51,25 +50,18 @@ class DockerComposer:
 
     def generate_compose_file(
             self,
-            device_type: str,
             template_file: str,
             output_file: str
     ) -> None:
         """generate_compose_file Render template_file to generate compose file.
 
         Args:
-            device_type (str): The target device for the docker compose file.
-                This device should have a section in settings.ini.
             template_file (str): Template file name without folder name.
                 Should be present in the templates folder.
             output_file (str): Output filename. Can be a path. Otherwise
                 file is created in current folder.
         """
         template = self.jinja_env.get_template(template_file)
-        if not self.config.has_section(f'device_{device_type}'):
-            print(f"Error: Unknown device type: {device_type}. "
-                  "No matching config section found in settings.ini")
-            sys.exit(1)
 
         template_args = {}
 
@@ -81,12 +73,6 @@ class DockerComposer:
 
         template_args["ENV"] = os.environ
 
-        for k, v in self.config['quectel_modem'].items():
-            template_args[k] = v
-
-        for k, v in self.config[f'device_{device_type}'].items():
-            template_args[k] = v
-
         output = template.render(**template_args)
         with open(output_file, 'w') as template_output:
             template_output.write(output)
@@ -95,9 +81,7 @@ class DockerComposer:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser("Generate docker-compose.yml.")
-    parser.add_argument('device_type',
-                        help="Target device. Must have a section in "
-                             "settings.ini.")
+
     parser.add_argument('--template', '-t', default='docker-compose.template',
                         help="Input template file. Should be present in "
                              "templates folder.")
@@ -107,5 +91,4 @@ if __name__ == '__main__':
     # print(args)
 
     composer = DockerComposer()
-    composer.generate_compose_file(
-        args.device_type, args.template, args.output)
+    composer.generate_compose_file(args.template, args.output)
