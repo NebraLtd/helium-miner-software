cd4d757b43ee94a6b8b93b81d18ea0e4a0ea417a
Murat Ursavas <mursavas@nebra.com>
2022-06-15 11:29:54 +0300

    Fleet deployment updated to the new naming style
    All necessary information is now taken from hm-pyhelper.
    Removed settings.ini dependency for devices and variants.
    Now generating all docker compose files for all new style variants.

--- gen_docker_compose.py
+++ gen_docker_compose.py
@@ -9,10 +9,6 @@ That's probably because output for `docker-compose config` does not retain the
 order of the sections present in the file. So as a workaround this small file
 tries to do the same thing but without re-arranging sections and just replacing
 placeholder values.
-
-In future if desired this can use a proper templating system like mako or jinja2.
-For the time being to avoid adding dependencies to the project it just uses python
-format strings.
 """
 
 import sys
@@ -20,6 +16,7 @@ import os
 import argparse
 from typing import Union
 from configparser import ConfigParser
+from hm_pyhelper.hardware_definitions import variant_definitions
 
 from jinja2 import Environment, select_autoescape, FileSystemLoader
 
@@ -51,61 +48,124 @@ class DockerComposer:
 
     def generate_compose_file(
             self,
-            device_type: str,
+            variant_type: str,
             template_file: str,
-            output_file: str
+            output_file: str,
+            generate_all: str,
+            output_folder: str,
     ) -> None:
         """generate_compose_file Render template_file to generate compose file.
 
         Args:
-            device_type (str): The target device for the docker compose file.
-                This device should have a section in settings.ini.
+            variant (str): The target variant for the docker compose file.
+                This variant has to have a definition in hm-pyhelper hardware-definitions.
             template_file (str): Template file name without folder name.
                 Should be present in the templates folder.
             output_file (str): Output filename. Can be a path. Otherwise
                 file is created in current folder.
+            generate_all (bool): Indicates whether a bulk generation has been requested or not
+            output_folder (str): Output folder for bulk generation. Relevant if
+                generate_all is true.
         """
         template = self.jinja_env.get_template(template_file)
-        if not self.config.has_section(f'device_{device_type}'):
-            print(f"Error: Unknown device type: {device_type}. "
-                  "No matching config section found in settings.ini")
-            sys.exit(1)
-
-        template_args = {}
 
         if 'versions' not in self.config:
             raise RuntimeError("Bad config, no [versions] section found")
 
-        for k, v in self.config['versions'].items():
-            template_args[k] = v
+        if generate_all is False:
+            if variant_type is None:
+                print("Error: Variant parameter is required.")
+                sys.exit(1)
 
-        template_args["ENV"] = os.environ
+            try:
+                variant_params = variant_definitions[variant_type]
+            except Exception:
+                print(f"Error: Unknown variant type: {variant_type}. "
+                      "No matching variant definition found in hm-pyhelper/hardware_definitions.")
+                sys.exit(2)
 
-        for k, v in self.config['quectel_modem'].items():
-            template_args[k] = v
+            template_args = {}
 
-        for k, v in self.config[f'device_{device_type}'].items():
-            template_args[k] = v
+            for k, v in self.config['versions'].items():
+                template_args[k] = v
 
-        output = template.render(**template_args)
-        with open(output_file, 'w') as template_output:
-            template_output.write(output)
+            template_args["ENV"] = os.environ
+
+            for k, v in self.config['quectel_modem'].items():
+                template_args[k] = v
+
+            try:
+                template_args['I2C_DEVICE'] = variant_params['KEY_STORAGE_BUS']
+            except Exception:
+                print("Error: Variant does not have a KEY_STORAGE_BUS information.")
+                sys.exit(3)
+
+            try:
+                template_args['ARCH'] = variant_params['CPU_ARCH']
+            except Exception:
+                print("Error: Variant does not have a CPU_ARCH information.")
+                sys.exit(4)
+
+            output = template.render(**template_args)
+            with open(output_file, 'w') as template_output:
+                template_output.write(output)
+
+            print(f"Docker compose file for {variant_type} has been generated successfully.")
+
+        else:
+            if not os.path.exists(output_folder):
+                os.mkdir(output_folder)
+
+            new_style_variants = \
+                [variant_type for variant_type in variant_definitions if variant_type.islower()]
+
+            for variant_type in new_style_variants:
+                variant_params = variant_definitions[variant_type]
+                template_args = {}
+
+                for k, v in self.config['versions'].items():
+                    template_args[k] = v
+
+                template_args["ENV"] = os.environ
+
+                for k, v in self.config['quectel_modem'].items():
+                    template_args[k] = v
+
+                try:
+                    template_args['I2C_DEVICE'] = variant_params['KEY_STORAGE_BUS']
+                except Exception:
+                    print("Error: Variant does not have a KEY_STORAGE_BUS information.")
+                    sys.exit(5)
+
+                try:
+                    template_args['ARCH'] = variant_params['CPU_ARCH']
+                except Exception:
+                    print("Error: Variant does not have a CPU_ARCH information.")
+                    sys.exit(6)
+
+                output = template.render(**template_args)
+                output_file = f"{output_folder}/docker-compose-{variant_type}.yml"
+                with open(output_file, 'w') as template_output:
+                    template_output.write(output)
+
+            print("Docker compose files for all variants have been generated successfully.")
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser("Generate docker-compose.yml.")
-    parser.add_argument('device_type',
-                        help="Target device. Must have a section in "
-                             "settings.ini.")
+    parser.add_argument('--variant', '-v',
+                        help="Target variant. Must have been defined in hm-pyhelper.")
     parser.add_argument('--template', '-t', default='docker-compose.template',
-                        help="Input template file. Should be present in "
-                             "templates folder.")
+                        help="Input template file. Should be present in templates folder.")
     parser.add_argument('--output', '-o', default='docker-compose.yml',
                         help="Output file. Created in current folder.")
+    parser.add_argument('--all', '-a', action='store_true',
+                        help="Generate compose files for all variants.")
+    parser.add_argument('--folder', '-f', default='device-compose-files',
+                        help="Output folder for the new compose files of all variants.")
     args = parser.parse_args()
-    # print(args)
 
     composer = DockerComposer()
     composer.generate_compose_file(
-        args.device_type, args.template, args.output)
+        args.variant, args.template, args.output, args.all, args.folder)
